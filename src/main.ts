dd5c14bc424d07263e5e39de83769396fede0aa4
Daniel Mastrapa <jmastrapa96@gmail.com>
2025-08-08 14:41:27 -0400

    [feature] rename "ordenes" module to "orders" and update dependencies and references, add swagger.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './filter/http.exception';
-import { TraceInterceptor } from './modules/application/interceptor/trace.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +15,18 @@ async function bootstrap() {
       transformOptions: { enableImplicitConversion: true },
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Servicio de Recepción de Órdenes')
+    .setDescription(
+      'Se requiere la implementación de un servicio que, a través de peticiones REST sobre HTTP, recepcione y procese solicitudes de impresión de documentos digitales.',
+    )
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, documentFactory);
+
   app.useGlobalFilters(new HttpExceptionFilter());
   // app.useGlobalInterceptors(new TraceInterceptor());
   await app.listen(process.env.NEST_PORT_APP ?? 3000);
